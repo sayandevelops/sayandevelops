435ff35f22220cd3e2e63a89327ab5f4a9a3be89
Sayan Mondal <sayon8023@gmail.com>
2025-06-29 08:51:46 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,18 +3,13 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// --- IMPORTANT ---
-// For demonstration purposes, these Firebase config values are hardcoded below.
-// For a real application, and for your Vercel deployment to work, you MUST
-// move these to Environment Variables as previously instructed.
-
 const firebaseConfig = {
   apiKey: "AIzaSyA67-Fy4JXZcq0obnckBoOVuowvn3vykjc",
   authDomain: "myauthproject-70650.firebaseapp.com",
   projectId: "myauthproject-70650",
   storageBucket: "myauthproject-70650.appspot.com",
   messagingSenderId: "662773446818",
-  appId: "1:662773446818:web:006a8ff87790acd4f2fffb"
+  appId: "1:662773446818:web:006a8ff87790acd4f2fffb",
 };
 
 // Initialize Firebase
